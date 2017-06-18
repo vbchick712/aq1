69f1421c72422b196fc427faf4cdfe030abe98e7
Shannan Muench <shannan.muench@gmail.com>
2017-06-18 18:41:24 -0400

    logging in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,12 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+  	@user = User.find_by(name: params[:name]).try(:authenticate, params[:password])
+  	 return render action: 'new' unless @user
+
+  	 session[:user_id] = @user.id
+  	 redirect_to quests_path
+  	end
 end
