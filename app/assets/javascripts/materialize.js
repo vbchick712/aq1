fe9782ae1d1de1875fd754f35e230c21f251a3dd
Misael Bejarano <vonleny@gmail.com>
2017-06-23 16:20:38 -0400

    Selecting cards and congratulations

--- app/assets/javascripts/materialize.js
+++ app/assets/javascripts/materialize.js
@@ -11,19 +11,52 @@ $( document ).on('turbolinks:load', function(){
 
 var card_array = [];
 var i = 0;
+var k = $('.card').length;
 
   $('#show_img').click(function(event){
   	event.preventDefault();
-  	// console.log($('.card').length);
+  	// $('.card')[0].css('display','inline');
   	// alert (i.value);
-  	// console.log($('.card')[0]); 
-	  	
-	card_array.push($('.card')[i]);
-	console.log(card_array);
-	console.log("active");
-	  		
-	i++;
-console.log(i);
+  	// console.log(card_array);
+    // console.log($('.quest')[i]); 
+     console.log($('.card').length); 
+	if (i < $('.card').length){
+		if (card_array.length == 0){
+			card_array.push($('.quest')[i]);
+			card_array[i].setAttribute('class', 'active');
+  		}
+  		else{
+
+  			var j = i-1;
+  			card_array[j].setAttribute('class', 'quest');
+  			card_array.splice(0,0);
+  			card_array.push($('.quest')[i]);
+  			card_array[i].setAttribute('class', 'active');
+  		}
+		i++ ;
+		
+		
+		// console.log("i:" + i);
+		// if (i != 0){
+		// 	var j = i - 1;
+		// 	card_array[j].setAttribute('class', 'quest');
+		// 	console.log("j: " + j);
+		// }
+		
+		// console.log(card_array);
+		console.log("I:"+i);
+		// console.log(k);
+		// console.log(j);
+
+	}else{
+		var j = i-1;
+		alert("a");
+		card_array[j].setAttribute('class', 'quest');
+  		$('.message').css('display','block');
+	} 		
+	
+	
+// console.log(i);
 	  	 	
   });
 
