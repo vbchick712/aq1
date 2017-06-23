0a7bd1ec870b663ebc8af475fa593cd5b39e82bf
Misael Bejarano <vonleny@gmail.com>
2017-06-23 12:02:58 -0400

    Creting jquery function to switch tasks in showtime.html.erd

--- app/assets/javascripts/materialize.js
+++ app/assets/javascripts/materialize.js
@@ -1,4 +1,4 @@
-$( document ).on('turbolinks:load', function() {
+$( document ).on('turbolinks:load', function(){
   $('.datepicker').pickadate({
     selectMonths: true, // Creates a dropdown to control month
     selectYears: 15 // Creates a dropdown of 15 years to control year
@@ -8,4 +8,23 @@ $( document ).on('turbolinks:load', function() {
 
   });
   $(".button-collapse").sideNav();
+
+var card_array = [];
+var i = 0;
+
+  $('#show_img').click(function(event){
+  	event.preventDefault();
+  	// console.log($('.card').length);
+  	// alert (i.value);
+  	// console.log($('.card')[0]); 
+	  	
+	card_array.push($('.card')[i]);
+	console.log(card_array);
+	console.log("active");
+	  		
+	i++;
+console.log(i);
+	  	 	
+  });
+
 });
