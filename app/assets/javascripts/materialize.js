85d0af581d404a415a02afbe83122fd569c69f32
Misael Bejarano <vonleny@gmail.com>
2017-06-24 10:26:53 -0400

    Getting showtime's tasks working

--- app/assets/javascripts/materialize.js
+++ app/assets/javascripts/materialize.js
@@ -9,55 +9,33 @@ $( document ).on('turbolinks:load', function(){
   });
   $(".button-collapse").sideNav();
 
-var card_array = [];
-var i = 0;
-var k = $('.card').length;
-
+  var card_array = [];
+  var i = 0;
+  var k = $('.card').length;
   $('#show_img').click(function(event){
   	event.preventDefault();
-  	// $('.card')[0].css('display','inline');
-  	// alert (i.value);
-  	// console.log(card_array);
-    // console.log($('.quest')[i]); 
-     console.log($('.card').length); 
+  	$('#show_img').text('check for answer');
+    // console.log($('.card').length); 
 	if (i < $('.card').length){
-		if (card_array.length == 0){
-			card_array.push($('.quest')[i]);
-			card_array[i].setAttribute('class', 'active');
-  		}
-  		else{
-
-  			var j = i-1;
-  			card_array[j].setAttribute('class', 'quest');
-  			card_array.splice(0,0);
-  			card_array.push($('.quest')[i]);
-  			card_array[i].setAttribute('class', 'active');
-  		}
-		i++ ;
-		
-		
-		// console.log("i:" + i);
-		// if (i != 0){
-		// 	var j = i - 1;
-		// 	card_array[j].setAttribute('class', 'quest');
-		// 	console.log("j: " + j);
-		// }
-		
-		// console.log(card_array);
-		console.log("I:"+i);
-		// console.log(k);
-		// console.log(j);
-
+	  if (card_array.length == 0){
+	    card_array.push($('.quest')[i]);
+		card_array[i].setAttribute('class', 'active');
+  	  }else{
+  		var j = i-1;
+  		card_array[j].setAttribute('class', 'quest');
+  		card_array.splice(0,0);
+  		card_array.push($('.quest')[i]);
+  		card_array[i].setAttribute('class', 'active');
+  	  }
+	  i++ ;
 	}else{
-		var j = i-1;
-		alert("a");
-		card_array[j].setAttribute('class', 'quest');
-  		$('.message').css('display','block');
-	} 		
-	
-	
-// console.log(i);
-	  	 	
+	  var j = i-1;
+	  card_array[j].setAttribute('class', 'quest');
+  	  $('.message').css('display','block');
+  	  // $('#show_img').attr('onClick', "document.location.href='/quests'");
+  	  $('.home1234').css('display', 'block');
+  	  $('#show_img').css('display', 'none');
+  	  
+	};		
   });
-
 });
