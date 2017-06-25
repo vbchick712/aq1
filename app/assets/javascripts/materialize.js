8bea77fee6abfe14af8283019b9f2dd175c5e212
Misael Bejarano <vonleny@gmail.com>
2017-06-25 11:50:38 -0400

    Creating showtime file with all the tasks and 1 clue working

--- app/assets/javascripts/materialize.js
+++ app/assets/javascripts/materialize.js
@@ -13,15 +13,14 @@ $( document ).on('turbolinks:load', function(){
   var clue_array =[];
   var i = 0;
   var k = $('.card').length;
+// Task Show each event
   $('#show_img').click(function(event){
-   event.preventDefault();
-  	$('#show_img').text('check for answer');
-    // clue_array.push($('#answer')[0].value);
-    console.log($('#id')[0].value);
+    event.preventDefault();
+    $('.initial_box').css('display', 'none');
+    $('#show_img').text('Next Task');
+    $('#show_img').css('display', 'none');
+    $('.check_answer').css('display', 'block');
     var task_id = "answer" + $('#id')[0].value;
-    console.log(task_id);
-    // clue_array.push($('#answer')[0].value);
-     // console.log(clue_array);
 
   	if (i < $('.card').length){
   	  if (card_array.length == 0){
@@ -33,15 +32,40 @@ $( document ).on('turbolinks:load', function(){
     		card_array.splice(0,0);
     		card_array.push($('.quest')[i]);
     		card_array[i].setAttribute('class', 'active');
-    	  }
+    	}
   	  i++ ;
   	}else{
   	  var j = i-1;
   	  card_array[j].setAttribute('class', 'quest');
-    	  $('.message').css('display','block');
-    	  // $('#show_img').attr('onClick', "document.location.href='/quests'");
-    	  $('.home1234').css('display', 'block');
-    	  $('#show_img').css('display', 'none');	  
+    	$('.message').css('display','block');
+    	// $('#show_img').attr('onClick', "document.location.href='/quests'");
+    	$('.home1234').css('display', 'block');
+    	$('#show_img').css('display', 'none');	  
   	};		
   });
+    // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+    $('.check_answer').click(function(event){
+      event.preventDefault();
+      var $this = $(this);
+
+      if ( $this.closest('.card-content').find('#answer').val() === $this.closest('.card-content').find('#taskanswer').val() ){
+        $('.wrong').css('display', 'none');
+        $('.wrong').css('display', 'none');
+        $('.right1').css('display', 'block');
+        $('.right1').css('display', 'block');
+        $('#modal1').modal();
+        $('.check_answer').css('display', 'none');
+        $('#show_img').css('display', 'block');
+        $('.help').fadeOut('slow');
+       }else{
+        $('.help').fadeIn('slow');
+        $('.right1').css('display', 'none');
+        $('.right1').css('display', 'none');
+        $('.wrong').css('display', 'block');
+        $('.wrong').css('display', 'block');
+        $('#modal1').modal();
+       }
+
+     });
 });
+
